14a03968023dd515b9796eb3884730ac674a4c9b
iyeonghun <twosoull@naver.com>
2024-08-16 16:41:32 +0900

    STEP 18) - 카프카 스케줄러 테스트

--- src/test/java/io/hhplus/concert/presentation/scheduler/OutBoxSchedulerTest.java
+++ src/test/java/io/hhplus/concert/presentation/scheduler/OutBoxSchedulerTest.java
@@ -2,13 +2,21 @@ package io.hhplus.concert.presentation.scheduler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.hhplus.concert.ConcertApplication;
+import io.hhplus.concert.common.enums.OutBoxStatus;
+import io.hhplus.concert.domain.entity.PaymentOutBox;
+import io.hhplus.concert.domain.respository.PaymentOutboxRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
@@ -19,9 +27,27 @@ public class OutBoxSchedulerTest {
     @Autowired
     OutBoxScheduler outBoxScheduler;
 
+    @Autowired
+    PaymentOutboxRepository paymentOutboxRepository;
+
     @Test
     public void OutBoxScheduler_test() throws JsonProcessingException {
+        // given
+        int initMessages = 4;
+        List<PaymentOutBox> outboxes = paymentOutboxRepository.findByStatus(OutBoxStatus.INIT);
+        assertThat(outboxes.size()).isEqualTo(initMessages);
+
+        // when
         outBoxScheduler.setPaymentOutBoxPublish();
+
+        // then
+        Awaitility.await()
+                .atMost(10, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    int publishedMessages = 4;
+                    List<PaymentOutBox> changedOutboxes = paymentOutboxRepository.findByStatus(OutBoxStatus.PUBLISH);
+                    assertThat(changedOutboxes.size()).isEqualTo(publishedMessages);
+        });
     }
 
 }
